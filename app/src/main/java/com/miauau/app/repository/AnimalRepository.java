722e4f11c245ad57f1f842e22684e3c73a8f7b5d
Michel Brito <michel.brito@inspira.com.br>
2024-09-18 17:50:38 -0300

    Ajusta tipo da chave primária recebida no AnimalRepository

--- app/src/main/java/com/miauau/app/repository/AnimalRepository.java
+++ app/src/main/java/com/miauau/app/repository/AnimalRepository.java
@@ -3,5 +3,7 @@ package com.miauau.app.repository;
 import com.miauau.app.entity.AnimalEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalRepository extends JpaRepository<AnimalEntity, Long> {
+import java.util.UUID;
+
+public interface AnimalRepository extends JpaRepository<AnimalEntity, UUID> {
 }
